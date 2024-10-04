95b855124f2a4c7485a62c15df7210dbae0418ca
HarshVardhan Gupta <154417829+9hvg@users.noreply.github.com>
2024-10-04 10:58:10 +0530

    Updates in some queries

--- Jobs_analysis.sql
+++ Jobs_analysis.sql
@@ -2,16 +2,14 @@
 -- queried using MySQL
 
 
-/*1. You're a Compensation analyst employed by a multinational corporation. Your Assignment is to Pinpoint Countries who give work fully remotely, for the title
- 'managers’ Paying salaries Exceeding $90,000 USD*/
-SELECT distINct(company_locatiON) 
+/*1. Pinpoint Countries who give work fully remotely, for the title 'managers’ Paying salaries Exceeding $90,000 USD*/
+SELECT DISTINCT(company_location) 
 FROM salaries 
 WHERE job_title like '%Manager%' and salary_IN_usd > 90000 and remote_ratio= 100;
 
 
-/*2. As a market researcher, your job is to Investigate the job market for a company that analyzes workforce data. Your Task is to know how many people were
- employed IN different types of companies AS per their size IN 2021.*/
-SELECT company_size, COUNT(company_size) AS 'COUNT of employees' 
+/*2. How many people were employed IN different types of companies AS per their size IN 2021.*/
+SELECT company_size, COUNT(company_size) AS 'Count of employees' 
 FROM salaries 
 WHERE work_year = 2021 
 GROUP BY company_size;
@@ -29,11 +27,11 @@ LIMIT 3;
 
 /*4. AS a remote work advocate Working for a progressive HR tech startup who place their freshers’ clients IN large tech firms. you're tasked WITH 
 Identifying top 5 Country Having  greatest count of large(company size) number of companies.*/
-SELECT company_locatiON, COUNT(company_size) AS 'cnt' 
+SELECT company_location, COUNT(company_size) AS 'cnt' 
 FROM (
     SELECT * FROM salaries WHERE experience_level ='EN' AND company_size='L'
 ) AS t  
-GROUP BY company_locatiON 
+GROUP BY company_location 
 ORDER BY cnt DESC
 LIMIT 5;
 
@@ -50,44 +48,41 @@ GROUP BY company_location;
 
 /*6. Picture yourself AS a data scientist Working for a workforce management platform. Your objective is to calculate the percentage of employees. 
 Who enjoy fully remote roles WITH salaries Exceeding $100,000 USD, Shedding light ON the attractiveness of high-paying remote positions IN today's job market.*/
-set @COUNT= (SELECT COUNT(*) FROM salaries  WHERE salary_IN_usd >100000 and remote_ratio=100);
+set @COUNTS= (SELECT COUNT(*) FROM salaries  WHERE salary_IN_usd >100000 and remote_ratio=100);
 set @total = (SELECT COUNT(*) FROM salaries where salary_in_usd>100000);
-set @percentage= round((((SELECT @COUNT)/(SELECT @total))*100),2);
-SELECT @percentage AS '%  of people workINg remotly and havINg salary >100,000 USD';
+set @percentage= round((((SELECT @COUNTS)/(SELECT @total))*100),2);
+SELECT @percentage AS '%  of people working remotely and having salary >100,000 USD';
 
 
 
 /*7. Imagine you're a data analyst Working for a global recruitment agency. Your Task is to identify the Locations where entry-level average salaries exceed the 
 average salary for that job title in market for entry level, helping your agency guide candidates towards lucrative countries.*/
-SELECT company_locatiON, t.job_title, average_per_country, average FROM 
+SELECT company_location, t.job_title, average_per_country, average FROM 
 (
-	SELECT company_locatiON,job_title,AVG(salary_IN_usd) AS average_per_country FROM  salaries WHERE experience_level = 'EN' 
-	GROUP BY  company_locatiON, job_title
+	SELECT company_location,job_title,AVG(salary_IN_usd) AS average_per_country FROM  salaries WHERE experience_level = 'EN' 
+	GROUP BY  company_location, job_title
 ) AS t 
 INNER JOIN 
 ( 
 	 SELECT job_title,AVG(salary_IN_usd) AS average FROM  salaries  WHERE experience_level = 'EN'  GROUP BY job_title
 ) AS p 
-ON  t.job_title = p.job_title WHERE average_per_country> average
+ON  t.job_title = p.job_title WHERE average_per_country > average
     
-
-
 
 
 /*8. You've been hired by a big HR Consultancy to look at how much people get paid IN different Countries. Your job is to Find out for each job title which
 Country pays the maximum average salary. This helps you to place your candidates IN those countries.*/
 
-SELECT company_locatiON , job_title , average FROM
+SELECT company_location , job_title , average FROM
 (
-SELECT *, dense_rank() over (partitiON by job_title order by average desc)  AS num FROM 
+SELECT *, dense_rank() over (partition by job_title order by average desc)  AS num FROM 
 (
-SELECT company_locatiON , job_title , AVG(salary_IN_usd) AS 'average' FROM salaries GROUP BY company_locatiON, job_title
+SELECT company_location , job_title , AVG(salary_IN_usd) AS 'average' FROM salaries GROUP BY company_location, job_title
 )k
 )t  WHERE num=1
 
 
 
- 
  /* 9.	Picture yourself AS a workforce strategist employed by a global HR tech startup. Your missiON is to determINe the percentage of  fully remote work for each 
  experience level IN 2021 and compare it WITH the correspONdINg figures for 2024, highlightINg any significant INcreASes or decreASes IN remote work adoptiON
  over the years.*/
